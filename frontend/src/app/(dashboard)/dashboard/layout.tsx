237b3ac014e739afb984aed4bc5d7581c62a7738
Worty <lethanhdat762003@gmail.com>
2025-03-24 18:18:17 +0700

    fix: remove chores

--- frontend/src/app/(dashboard)/dashboard/layout.tsx
+++ frontend/src/app/(dashboard)/dashboard/layout.tsx
@@ -13,15 +13,8 @@ import {
   Users,
 } from "lucide-react";
 
-import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+
 import {
   DropdownMenu,
   DropdownMenuContent,
