d1f84eeda6308ede33b2e5bdfafc447840f01335
Worty <lethanhdat762003@gmail.com>
2025-04-02 15:29:39 +0700

    chore: fix lack of props

--- frontend/src/components/main/News.tsx
+++ frontend/src/components/main/News.tsx
@@ -8,7 +8,12 @@ export default function ChampionsPage() {
       className="py-10 sm:py-12 md:py-16 lg:py-20 px-4 sm:px-6 md:px-8"
       style={{ backgroundImage: 'url("/images/testimonial-bg.png")' }}
     >
-      <Title subtitle="Luxury" title="ABOUT" />
+      <Title
+        subtitle="Luxury"
+        title="ABOUT"
+        subtitleColor="text-primary"
+        titleColor="text-black"
+      />
       <FeaturedPosts />
     </div>
   );
